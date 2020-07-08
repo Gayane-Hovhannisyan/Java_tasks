d0b8e65857ae45119fb31ed227e835fce61ab4d2
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-08 17:38:21 +0400

    mathematical functions  exercise about arctan and root

--- mathfunction3.java
+++ mathfunction3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class mathfunction2 {
+public class mathfunction3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in) ;
         System.out.print("x = ") ;
