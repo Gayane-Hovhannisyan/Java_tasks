d66dca0e697b0ac4203b949ac318d50d7c2f7f45
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-15 20:27:36 +0400

    added for in for  exercise, which solved with method

--- loops/forloops3.java
+++ loops/forloops3.java
@@ -1,18 +1,33 @@
 public class forloops3 {
     public static void main(String[] args) {
         exercise141(1);
+
+        exercise143(1);
     }
 
     public static void exercise141(double a) {
         double S = 0;
         double P = 1;
         for (int m = 0; m <= 5; m++) {
-            
+
             for (int n = 0; n <= 6; n++) {
                 S = S + Math.pow(a, m + n);
             }
             P = P * S;
             S = 0;
+        }
+        System.out.println("P = " + P);
+    }
+
+    public static void exercise143(double n) {
+        double S = 1;
+        double P = 1;
+        for (int i = 0; i <= 15; i++) {
+            for (int m = 1; m <= 6; m++) {
+                S = S * (i / (i + m * n));
+            }
+            P = P * S;
+            S = 1;
         }
         System.out.println("P = " + P);
     }
