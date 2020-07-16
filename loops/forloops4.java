3292963aa3c6d0d8368ed1b011f977abcdc2d670
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-16 12:39:32 +0400

    program is about for loop

---
+++ loops/forloops4.java
@@ -0,0 +1,28 @@
+public class forloops4 {
+    public static void main(String[] args) {
+       // exercise151(6);
+
+        exercise154(100);
+    }
+
+    public static void exercise151(int n) {
+        int S = 0;
+        for (int i = 1; i < n / 2; i++) {
+            if (n % i == 0) {
+                S = S + i + n / i;
+            }
+        }
+
+        System.out.println("S = " + S);
+    }
+
+    public static void exercise154(int n) {
+        int P = 1;
+        for (int i = 1; i < n / 2; i++) {
+            if (n % i == 3) {
+                P = P * (i + n / i);
+            }
+        }
+        System.out.println("P = " + P);
+    }
+}
