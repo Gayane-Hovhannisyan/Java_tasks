64f0eba699879e2e190e4f27f2fb49d773693f40
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-18 23:32:49 +0400

    added one exercise

--- loops/forloops5.java
+++ loops/forloops5.java
@@ -1,6 +1,11 @@
 public class forloops5 {
   public static void main(String[] args) {
+
     exercise167(5);
+
+    int z = exercise169(4, 4);
+    System.out.println("z = " + z);
+
   }
 
   public static void exercise167(double x) {
@@ -19,6 +24,24 @@ public class forloops5 {
       y = false;
     }
     System.out.println("y = " + y);
+  }
+
+  public static int exercise169(int x, int y) {
+    int z, a;
+    int k = 0;
+    a = x + y;
+    for (int i = 2; i < a / 2; i++) {
+      if (a % i == 0) {
+        k = k + 1;
+      }
+    }
+    if (k == 0) {
+      z = 5;
+    } else {
+      z = 6;
+    }
+
+    return z;
   }
 
 }
