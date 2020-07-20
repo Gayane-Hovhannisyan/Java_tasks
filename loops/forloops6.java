0c05fcf0db143e84b23ce77b5bd8db6ba619675e
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-20 17:40:32 +0400

    added exercise

---
+++ loops/forloops6.java
@@ -0,0 +1,11 @@
+public class forloops6 {
+    public static void main(String[] args) {
+        exercise181(16);
+    }
+
+    public static void exercise181(int N) {
+     
+        int m = (int)(Math.log(N) / Math.log(2));
+        System.out.println(m);
+    }
+}
