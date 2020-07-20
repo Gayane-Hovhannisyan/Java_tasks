805e8105bf13a4c80422a760169f62e62275871b
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-20 13:18:48 +0400

    added exercise which solved with method

--- loops/forloops4.java
+++ loops/forloops4.java
@@ -1,14 +1,16 @@
 public class forloops4 {
     public static void main(String[] args) {
-        exercise151(6);
+        // exercise151(6);
 
-        exercise154(10);
+        // exercise154(10);
 
-        exercise156();
+        // exercise156();
 
-        exercise159();
+        // exercise159();
 
-        exercise164(20);
+        exercise162();
+
+        // exercise164(20);
 
     }
 
@@ -56,12 +58,24 @@ public class forloops4 {
         System.out.println("P = " + P);
     }
 
-    
+    public static void exercise162() {
+
+        double m, S;
+        for (int i = 9999; i > 999; i--) {
+            S = 14 * i;
+            m = Math.sqrt(S);
+            if (m / 1 == Math.floor(m)) {
+                System.out.println("i = " + i);
+                return;
+            }
+
+        }
+    }
 
     public static void exercise164(int n) {
-        for(int i = 10; i < 32; i++) {
-            if(i > n) {
-                System.out.println("value = " + i*i);
+        for (int i = 10; i < 32; i++) {
+            if (i > n) {
+                System.out.println("value = " + i * i);
                 return;
             }
         }
