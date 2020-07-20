4e72be1aa590a6366525bf61f7d50a07b7ce68aa
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-20 19:48:52 +0400

    added program about math.pow and for loop

--- loops/forloops6.java
+++ loops/forloops6.java
@@ -1,11 +1,25 @@
 public class forloops6 {
     public static void main(String[] args) {
-        exercise181(16);
+        xercise181(16);
+
+        exercise183(35);
     }
 
     public static void exercise181(int N) {
      
         int m = (int)(Math.log(N) / Math.log(2));
         System.out.println(m);
+    }
+
+    public static void exercise183(int N) {
+        double a;
+
+        for(int i = 1; i <= N; i++) {
+            a = Math.pow(3, i);
+            if(a > N) {
+                System.out.println("K = " + i); 
+                return;
+            }
+        }
     }
 }
