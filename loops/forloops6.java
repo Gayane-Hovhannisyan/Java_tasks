e491e72d0901586c9331fca78df39d96b4c9aaa2
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-21 23:59:08 +0400

    this program includes 5 exercises

--- loops/forloops6.java
+++ loops/forloops6.java
@@ -1,10 +1,14 @@
 public class forloops6 {
     public static void main(String[] args) {
-        exrcise181(16);
+         exrcise181(16);
 
-        exercise183(35);
+         exercise183(35);
 
-        exercise185(20);
+         exercise185(20);
+
+         exercise188(8);
+
+         exercise190(0.000001);
 
     }
 
@@ -40,4 +44,38 @@ public class forloops6 {
         System.out.println("month = " + k);
     }
 
+    public static void exercise188(int N) {
+        boolean y = false;
+        int a = 0;
+        int b = 0;
+        int c = 1;
+        for (int i = 1; i <= N; i++) {
+            a = b;
+            b = c;
+            c = a + b;
+            if (c == N) {
+                y = true;
+                break;
+            }
+
+        }
+        System.out.println("y = " + y);
+
+    }
+
+    public static void exercise190(double eps) {
+        double x1 = 1.0;
+        double x2 = 2.0;
+        double x3;
+        double k = 3;
+        
+        while(Math.abs(x2 - x1) > eps) {
+            x3 = (x1 + 2 * x2)/3; 
+            x1 = x2;
+            x2 = x3;
+            k = k + 1;
+
+        }
+        System.out.println("k = " + k);
+    }
 }
