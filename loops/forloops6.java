18b792f2fdd186b3b17f1621dd38c729dda20333
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-21 20:07:03 +0400

    added program about %

--- loops/forloops6.java
+++ loops/forloops6.java
@@ -1,25 +1,43 @@
 public class forloops6 {
     public static void main(String[] args) {
-        xercise181(16);
+        exrcise181(16);
 
         exercise183(35);
+
+        exercise185(20);
+
     }
 
     public static void exercise181(int N) {
-     
-        int m = (int)(Math.log(N) / Math.log(2));
+
+        int m = (int) (Math.log(N) / Math.log(2));
         System.out.println(m);
     }
 
     public static void exercise183(int N) {
         double a;
 
-        for(int i = 1; i <= N; i++) {
+        for (int i = 1; i <= N; i++) {
             a = Math.pow(3, i);
-            if(a > N) {
-                System.out.println("K = " + i); 
+            if (a > N) {
+                System.out.println("K = " + i);
                 return;
             }
         }
     }
+
+    public static void exercise185(int P) {
+        int a = 30000;
+        int k = 0;
+        if (P > 0 && P < 25) {
+            while (a < 100000) {
+                a = a + a * P / 100;
+                k = k + 1;
+            }
+        } else {
+            System.out.print("Enter correct number, please");
+        }
+        System.out.println("month = " + k);
+    }
+
 }
