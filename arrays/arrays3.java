a1d2427084c9122bf7334688ace58a302685da26
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-23 20:36:06 +0400

    added exercise about 1D array, number of zeros

--- arrays/arrays3.java
+++ arrays/arrays3.java
@@ -8,12 +8,14 @@ public class arrays3 {
         exercise234();
 
         exercise236();
+
+        exercise237();
     }
 
     public static void exercise231() {
-        int numbers[] = {12, 5, 48, -9, -1, 257, 31, 12, 4 };
+        int numbers[] = { 12, 5, 48, -9, -1, 257, 31, 12, 4 };
         double S = 0;
-        for(int i = 0; i < 9; i++) {
+        for (int i = 0; i < 9; i++) {
             if (numbers[i] % 2 == 0) {
                 S = S + Math.pow(numbers[i], 2);
             }
@@ -23,10 +25,10 @@ public class arrays3 {
     }
 
     public static void exercise233() {
-        int numbers[] = {12, 5, 48, -9, -1, 257, 31, 12, 4 };
+        int numbers[] = { 12, 5, 48, -9, -1, 257, 31, 12, 4 };
         double S = 0;
         double P = 1;
-        for(int i = 0; i < 9; i++) {
+        for (int i = 0; i < 9; i++) {
             if (numbers[i] % 2 == 0) {
                 S = S + numbers[i];
                 P = P * numbers[i];
@@ -37,11 +39,11 @@ public class arrays3 {
     }
 
     public static void exercise234() {
-        int numbers[] = {12, 5, 48, -9, -1, 257, 31, 12, 4 }; 
+        int numbers[] = { 12, 5, 48, -9, -1, 257, 31, 12, 4 };
         int m = 0;
         double S = 0;
-        for(int i = 0; i< 9; i++) {
-            if(numbers[i] % 2 == 1) {
+        for (int i = 0; i < 9; i++) {
+            if (numbers[i] % 2 == 1) {
                 m = m + 1;
                 S = S + numbers[i];
             }
@@ -51,16 +53,28 @@ public class arrays3 {
     }
 
     public static void exercise236() {
-        int numbers[] = {12, 5, 48, -9, -1, 257, 31, 12, 4 };
+        int numbers[] = { 12, 5, 48, -9, -1, 257, 31, 12, 4 };
         double m = 0;
         double P = 1;
-        for(int i = 0; i < 9; i++) {
+        for (int i = 0; i < 9; i++) {
             if (numbers[i] % 2 == 0) {
                 m = m + 1;
                 P = P * numbers[i];
             }
         }
         System.out.println("m = " + m + "   P = " + P);
+
+    }
+
+    public static void exercise237() {
+        int numbers[] = { 0, 5, 48, 0, -1, 257, 0, 0, 4 };
+        int m = 0;
+        for (int i = 0; i < 9; i++) {
+            if (numbers[i] == 0) {
+                m = m + 1;
+            }
+        }
+        System.out.println("m = " + m);
 
     }
 }
