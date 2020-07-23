667bfb47ca71a36ceb24e242c3673bf523432888
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-23 20:03:56 +0400

    added exercise about 1D array

---
+++ arrays/arrays3.java
@@ -0,0 +1,17 @@
+public class arrays3 {
+    public static void main(String[] args) {
+        exercise231();
+    }
+
+    public static void exercise231() {
+        int numbers[] = {12, 5, 48, -9, -1, 257, 31, 12, 4 };
+        double S = 0;
+        for(int i = 0; i < 9; i++) {
+            if (numbers[i] % 2 == 0) {
+                S = S + Math.pow(numbers[i], 2);
+            }
+        }
+        System.out.println("S = " + S);
+
+    }
+}
