8d5aba0afd55ce75ec626f57a01515f3f2116236
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-23 14:54:49 +0400

    added exercise about 1D array and middle square

--- arrays/arrays1.java
+++ arrays/arrays1.java
@@ -2,19 +2,35 @@ public class arrays1 {
     public static void main(String[] args) {
         exercise211();
 
+        exercise213();
+
     }
 
     public static void exercise211() {
-        int numbers [] = {1, 4, -58, 12,-12 };
+        int numbers[] = { 1, 4, -58, 12, -12 };
         double S = 0;
         int k = 0;
-        for(int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++) {
             if (numbers[i] > 0) {
                 S = S + numbers[i];
                 k = k + 1;
             }
         }
         S = S / k;
+        System.out.println("S = " + S);
+    }
+
+    public static void exercise213() {
+        int numbers[] = { -10, -7, 2, 1, -3, -1, 3 };
+        double S = 0;
+        int k = 0;
+        for (int i = 0; i < 7; i++) {
+            if (numbers[i] < 0) {
+                S = S + Math.pow(numbers[i], 2);
+                k = k + 1;
+            }
+        }
+        S = Math.sqrt(S) / k;
         System.out.println("S = " + S);
     }
 }
