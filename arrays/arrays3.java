37c3f5eb2d4a670ca39ca369d1352f7a2913e929
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-23 20:17:26 +0400

    added exercise about 1D array, arithmetic average

--- arrays/arrays3.java
+++ arrays/arrays3.java
@@ -1,8 +1,11 @@
+
 public class arrays3 {
     public static void main(String[] args) {
         exercise231();
 
         exercise233();
+
+        exercise234();
     }
 
     public static void exercise231() {
@@ -29,5 +32,19 @@ public class arrays3 {
         }
         System.out.println("S = " + S + "   P = " + P);
 
+    }
+
+    public static void exercise234() {
+        int numbers[] = {12, 5, 48, -9, -1, 257, 31, 12, 4 }; 
+        int m = 0;
+        double S = 0;
+        for(int i = 0; i< 9; i++) {
+            if(numbers[i] % 2 == 1) {
+                m = m + 1;
+                S = S + numbers[i];
+            }
+        }
+        S = S / m;
+        System.out.println("S = " + S);
     }
 }
