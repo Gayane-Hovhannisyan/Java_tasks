250b883b3ae77d9506dda762b464621b1a6f31d8
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-24 13:34:05 +0400

    added exercise about 1D arrat, sum, mod

--- arrays/arrays4.java
+++ arrays/arrays4.java
@@ -1,9 +1,13 @@
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
 public class arrays4 {
     public static void main(String[] args) {
 
         exercise242(2);
 
         exercise244();
+
+        exercise245();
 
     }
 
@@ -22,10 +26,25 @@ public class arrays4 {
         int numbers[] = { 0, 5, 18, 6, 35, 9, -1, 27, 15, 115, 0, 0, 4 };
         int P = 1;
         for (int i = 0; i < 13; i++) {
-            if(numbers[i] % 5 == 2) {
+            if (numbers[i] % 5 == 2) {
                 P = P * numbers[i];
             }
         }
         System.out.println("P = " + P);
     }
+
+    public static void exercise245() {
+        int numbers[] = { 3, 5, 16, 6, 35, 9, -1, 27, 15, 115, 0, 0, 4 };
+        int S = 0;
+        int m;
+        for (int i = 0; i < 13; i++) {
+            m = i + numbers[i];
+            if (m % 3 == 0) {
+                S = S + numbers[i];
+            }
+        }
+        System.out.println("S = " + S);
+
+    }
+
 }
