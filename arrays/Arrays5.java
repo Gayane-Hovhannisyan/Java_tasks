6aaca38cf31983999d75ddeec219a4ef26946f9d
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-25 17:26:57 +0400

    added exercise 1D array, max item and random method

---
+++ arrays/Arrays5.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class Arrays5 {
+static int[] numbers;
+    public static void main(String[] args) {
+        int[] numbers = array(10);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("numbers[" + i + "]=" + numbers[i]);
+        }
+
+        exercise251();
+
+    }
+
+    public static int[] array(int n) {
+        Random rand = new Random();
+         numbers = new int[n];
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = rand.nextInt(100);
+
+        }
+        return numbers;
+    }
+
+    public static void exercise251() {
+
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        System.out.println("max = " + max);
+    }
+}
