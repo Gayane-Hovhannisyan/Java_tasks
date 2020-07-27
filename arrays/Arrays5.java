6be28eb3806018da886aaf5594b4e2eba70e4cbc
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-27 17:44:26 +0400

    added exercise about 1D array, min and max item

--- arrays/Arrays5.java
+++ arrays/Arrays5.java
@@ -1,7 +1,8 @@
 import java.util.Random;
 
 public class Arrays5 {
-static int[] numbers;
+    static int[] numbers;
+
     public static void main(String[] args) {
         int[] numbers = array(10);
         for (int i = 0; i < 10; i++) {
@@ -10,11 +11,13 @@ static int[] numbers;
 
         exercise251();
 
+        exercise253();
+
     }
 
     public static int[] array(int n) {
         Random rand = new Random();
-         numbers = new int[n];
+        numbers = new int[n];
         for (int i = 0; i < 10; i++) {
             numbers[i] = rand.nextInt(100);
 
@@ -32,4 +35,20 @@ static int[] numbers;
         }
         System.out.println("max = " + max);
     }
+
+    public static void exercise253() {
+        int max = numbers[0];
+        int min = numbers[0];
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] > max) {
+                max = numbers[i];
+            } else {
+                if(numbers[i] < min) {
+                    min = numbers[i];
+                }
+            }
+        }
+        System.out.println("max = " + max + "   min = " + min);
+}
+
 }
