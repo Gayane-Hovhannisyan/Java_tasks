aeb4fb71981dc98d1800c78ae586bb43de5eacf1
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-07-30 23:36:51 +0400

    added exercise about 1D array

--- arrays/Arrays1.java
+++ arrays/Arrays1.java
@@ -8,6 +8,8 @@ public class Arrays1 {
 
         exercise213();
 
+        exercise214();
+
         exercise215();
 
         exercise218();
@@ -57,6 +59,20 @@ public class Arrays1 {
             }
         }
         S = Math.sqrt(S) / k;
+        System.out.println("S = " + S);
+    }
+
+    public static void exercise214() {
+        int numbers[] = { 1, 4, -58, 12, -12 };
+        double S = 0;
+        int k = 0;
+        for (int i = 0; i < 5; i++) {
+            if (numbers[i] < 0) {
+                S = S + numbers[i];
+                k = k + 1;
+            }
+        }
+        S = S / k;
         System.out.println("S = " + S);
     }
 
