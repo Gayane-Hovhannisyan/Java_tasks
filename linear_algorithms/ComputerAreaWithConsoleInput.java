7bc279d31ec53ed40452ea6841d6a510f7456521
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-12-24 13:53:14 +0400

    added linear algorithm about average

--- linear_algorithms/ComputerAreaWithConsoleInput.java
+++ linear_algorithms/ComputerAreaWithConsoleInput.java
@@ -1,10 +1,19 @@
 import java.util.Scanner;
 public class ComputerAreaWithConsoleInput {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+       /* Scanner input = new Scanner(System.in);
         System.out.print("Enter a number for radius");
         double radius = input.nextDouble();
         double area = radius * radius * 3.14159;
-        System.out.println("The area the circle of radius " + radius + " is " + area);
+        System.out.println("The area the circle of radius " + radius + " is " + area); */
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter three numbers  ");
+        double num1 = input.nextDouble();
+        double num2 = input.nextDouble();
+        double num3 = input.nextDouble();
+
+        double average = (num1 + num2 + num3) / 3;
+        System.out.println("The average of " + num1 + ", " + num2 + ", " + num3 + " is " + average);
     }
 }
