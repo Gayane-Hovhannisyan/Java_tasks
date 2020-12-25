ee38194f875a4f29da8a4fd1fa799f1af0a64307
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-12-25 19:52:49 +0400

    added simple exercise to fix errors

--- linear_algorithms/ComputerAreaWithConsoleInput.java
+++ linear_algorithms/ComputerAreaWithConsoleInput.java
@@ -7,13 +7,17 @@ public class ComputerAreaWithConsoleInput {
         double area = radius * radius * 3.14159;
         System.out.println("The area the circle of radius " + radius + " is " + area); */
 
-        Scanner input = new Scanner(System.in);
+       /*  Scanner input = new Scanner(System.in);
         System.out.println("Enter three numbers  ");
         double num1 = input.nextDouble();
         double num2 = input.nextDouble();
         double num3 = input.nextDouble();
 
         double average = (num1 + num2 + num3) / 3;
-        System.out.println("The average of " + num1 + ", " + num2 + ", " + num3 + " is " + average);
+        System.out.println("The average of " + num1 + ", " + num2 + ", " + num3 + " is " + Descriptive  abbreviationsaverage); */
+
+         int i, j, k;
+         i = j = k = 2;
+        System.out.println(i + " " + j + " " + k + " ");
     }
 }
