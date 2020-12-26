32796dcfc13d08699e3fdcffd88eab8a3d6c6854
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2020-12-26 19:10:43 +0400

    added simple exercise about constant value

--- linear_algorithms/ComputerAreaWithConsoleInput.java
+++ linear_algorithms/ComputerAreaWithConsoleInput.java
@@ -20,8 +20,16 @@ public class ComputerAreaWithConsoleInput {
          i = j = k = 2;
         System.out.println(i + " " + j + " " + k + " "); */
 
-        int k = 0;
+        /* int k = 0;
         int i = k + 2;
-        System.out.println(i);
-    }
+        System.out.println(i); */
+
+        final double PI = 3.14159;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number for radius: ");
+        double radius = input.nextDouble();
+        double area = radius * radius * PI;
+
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
+        }
 }
