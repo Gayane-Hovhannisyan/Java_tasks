a694570dc9857e430a6066c54b41de78519a1ca3
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2021-01-01 23:32:30 +0400

    ..

--- arrays/Arrays3a.java
+++ arrays/Arrays3a.java
@@ -1,11 +1,12 @@
 import java.util.Random;
 
 public class Arrays3a {
+    static int[] numbers;
     public static void main(String[] args) {
 
         int[] numbers = array(10);
         for (int i = 0; i < 10; i++) {
-            System.out.println("numbers[" + i + "]=" + numbers[i]);
+            System.out.println("numbers[" + i + "]=" + numbers[i]);}
 
         exercise231();
 
@@ -16,7 +17,7 @@ public class Arrays3a {
         exercise238();
 
         exercise240(); */
-    }}
+    }
 
     public static int[] array(int n) {
         Random rand = new Random();
@@ -44,7 +45,7 @@ public class Arrays3a {
         int a = 0;
         for(int i = 0; i < numbers.length; i++) {
             if(numbers[i] % 2 == 0) {
-                a = a +1;
+                a = a + 1;
             }
         }
         System.out.println(a);
