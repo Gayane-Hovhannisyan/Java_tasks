7182e80a0c20f9a4f333297df5459ade3b6ed6dd
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2021-01-04 18:50:14 +0400

    ..

---
+++ arrays/Arrays4a.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+import java.util.Scanner;
+
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
+public class Arrays4a {
+    static int[] numbers;
+
+    public static void main(String[] args) {
+        int[] numbers = array(10);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("numbers[" + i + "]=" + numbers[i]);}
+
+            exercise241();
+            exercise243();
+            exercise245();
+            exercise247();
+            exercise249();
+            
+    }
+}
