ff75647a7fbe3c094dd614ebfbd658f1555cc384
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2021-01-04 18:58:01 +0400

    added exercise

--- arrays/Arrays4a.java
+++ arrays/Arrays4a.java
@@ -11,11 +11,25 @@ public class Arrays4a {
         for (int i = 0; i < 10; i++) {
             System.out.println("numbers[" + i + "]=" + numbers[i]);}
 
-            exercise241();
-            exercise243();
+            exercise241(5);
+
+            exercise243(8);
+
             exercise245();
-            exercise247();
-            exercise249();
             
+            exercise247();
+            
+            exercise249();
+
     }
+    public static void exercise241(int k) {
+        int S = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if(numbers[i] % k == 0) {
+                S = S + numbers[i];
+            }
+        }
+        System.out.println("S = " + S);
+    }
+
 }
