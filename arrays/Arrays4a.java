c768bec5b70445630d9e91cc7be92ddffd743a47
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2021-01-05 23:28:10 +0400

    ..

--- arrays/Arrays4a.java
+++ arrays/Arrays4a.java
@@ -1,8 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
 
-import jdk.javadoc.internal.doclets.formats.html.resources.standard;
-
 public class Arrays4a {
     static int[] numbers;
 
@@ -11,15 +9,15 @@ public class Arrays4a {
         for (int i = 0; i < 10; i++) {
             System.out.println("numbers[" + i + "]=" + numbers[i]);}
 
-            exercise241(5);
+        exercise241(5);
 
-            exercise243(8);
+        exercise243(8);
 
-            exercise245();
+        /*exercise245();
             
-            exercise247();
-            
-            exercise249();
+        exercise247();
+        
+        exercise249(); */
 
     }
     public static void exercise241(int k) {
@@ -32,4 +30,13 @@ public class Arrays4a {
         System.out.println("S = " + S);
     }
 
+    public static void exercise243(int t) {
+        int k = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % t == 0) {
+                k = k + 1;
+            }
+        }
+        System.out.println("k = " + k);
+    }
 }
