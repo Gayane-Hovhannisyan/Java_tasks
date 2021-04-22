845a40f47b0a638142865b479a2668e5e58be28a
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2021-04-22 15:20:17 +0400

    added book exercise

---
+++ book_exercises/Listing31.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Listing31 {
+    public static void main(String[] args) {
+       int number1 = (int) (System.currentTimeMillis() % 100);
+       int number2 = (int) (System.currentTimeMillis() / 100 % 100);
+       Scanner input = new Scanner(System.in);
+        System.out.print ( "What is " + number1 + " + " + number2 + " ? "); 
+        int answer = input.nextInt();
+        System.out.println (number1 + " + " + number2 + " = " + answer + " is " + (number1 + number2 == answer));
+    }
+} 
