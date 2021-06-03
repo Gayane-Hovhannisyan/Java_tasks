30645d87b63109ad7f2e837105af34abd1f36ef0
Gayane-Hovhannisyan <gayahovhanns@gmail.com>
2021-06-03 15:45:25 +0400

    nested loops

--- book_exercises/exerciseA.java
+++ book_exercises/exerciseA.java
@@ -1,6 +1,7 @@
 public class exerciseA {
     public static void main(String[] args) {
         a();
+        b();
     }
     public static void a () {
         for(int i = 0; i<6; i++) {
@@ -11,4 +12,13 @@ public class exerciseA {
 
         }
     }
+     public static void b() {
+         for (int i = 0; i<6; i++) {
+             for(int j = 0; j <= 6-1-i; j++) {
+                 System.out.print(" * ");
+             }
+             System.out.print("\n");
+         }
+
+     }
 }
